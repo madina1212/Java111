d299f07b91289d4e3d7914acf2558cbaeee59998
madina1212 <halikovamadina656@gmail.com>
2022-12-05 18:05:47 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,170 @@
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+//        Java 8 үчүн практикалык тапшырма
+//
+//        1. Жаңы массив тизмесин түзүү үчүн Java программасын жазыңыз,
+//        бир нече шаарларды  кошуп, аларды консольго чыгарыныз.
+//        ArrayList<String> strings = new ArrayList<>();
+//        strings.add("Bishkek");
+//        strings.add("Osh");
+//        strings.add("Kara-Kol");
+//        for (String str : strings) {
+//            System.out.println(str);
+//
+//
+//        2. Массив тизмесине элементти биринчи орунга киргизүү үчүн Java программасын жаз.
+//        ArrayList<String> names = new ArrayList<>();
+//        names.add("Madina");
+//        names.add("ainura");
+//        names.add("Begimjan");
+//        names.add("Albina");
+//        names.add(0,"Kanat");
+//        System.out.println(names);
+
+//
+//        3. Берилген массивдердин тизмесинен элементти (белгиленген индексте) алуу үчүн Java программасын жазыңыз.
+//            ArrayList<String> arrayList = new ArrayList<>();
+//            arrayList.add("Akylai");
+//            arrayList.add("Erbol");
+//            arrayList.add("Aigerim");
+//            arrayList.add("Sumaya");
+//            arrayList.add("Baktinisa");
+//            System.out.println(arrayList);
+//            System.out.println(arrayList.get(1));
+//
+//        4. Берилген элемент боюнча белгилүү бир массив элементин озгортуу үчүн Java программасын жазыңыз.
+//
+//        ArrayList<String> arrayList = new ArrayList<>();
+//        arrayList.add("Clean Code");
+//        arrayList.add("Clean Coder");
+//        arrayList.add("Effective Java");
+//        arrayList.add("Head first Java");
+//        arrayList.add("Head first design patterns");
+//
+//        // How, suppose you want to replace "Clean Coder" with
+//        // "Introduction to Algorithms"
+//        System.out.println("ArrayList before replace: "+arrayList);
+//
+//        arrayList.set(1,"Introduction to Algorithm");
+//
+//        System.out.println("ArrayList after replace: "+arrayList);
+
+
+//        5. Массив тизмесинен үчүнчү элементти очуруп салуу үчүн Java программасын жазыңыз.
+//        ArrayList<String> arrayList = new ArrayList<>();
+//        arrayList.add("Java-8");
+//        arrayList.add("Java-7");
+//        arrayList.add("Java-6");
+//        arrayList.add("Java-9");
+//        System.out.println(arrayList);
+//        arrayList.remove(3);
+//        System.out.println(arrayList);
+
+
+//        6. Массив тизмесиндеги элементти издөө үчүн Java программасын жазыңыз.
+//        ArrayList<Integer> number = new ArrayList<>();
+//        number.add(1);
+//        number.add(2);
+//        number.add(3);
+//        number.add(4);
+//        number.add(5);
+//        System.out.println(number.contains(2));
+
+//
+//        7. Берилген массивдердин тизмесин сорттоо үчүн Java программасын жазыңыз.
+//        ArrayList<Character> arrayList = new ArrayList<>();
+//        arrayList.add('e');
+//        arrayList.add('c');
+//        arrayList.add('b');
+//        arrayList.add('d');
+//        arrayList.add('a');
+//        System.out.println("arrayList before sort: "+arrayList);
+//        Collections.sort(arrayList);
+//        System.out.println("List after sort: "+arrayList);
+
+//
+//        8. Бир массив тизмесин экинчи масивге көчүрүү үчүн Java программасын жазыңыз.
+//        ArrayList<String> list1 = new ArrayList<String>();
+//        list1.add("1");
+//        list1.add("2");
+//        list1.add("3");
+//        list1.add("4");
+//        System.out.println("List1: " + list1);
+//        List<String> list2 = new ArrayList<String>();
+//        list2.add("A");
+//        list2.add("B");
+//        list2.add("C");
+//        list2.add("D");
+//        System.out.println("List2: " + list2);
+//        // Copy List2 to List1
+//        Collections.copy(list1, list2);
+//        System.out.println("Copy List to List2,\nAfter copy:");
+//        System.out.println("List1: " + list1);
+//        System.out.println("List2: " + list2);
+
+
+//        9. Массив тизмесиндеги элементтерди аралаштыруу үчүн Java программасын жазып.
+//        Маанилерин консольго чыгарыныз.
+//        ArrayList<String> arrayList = new ArrayList<>();
+//
+//        // Adding custom input elements to list object
+//        arrayList.add("code");
+//        arrayList.add("quiz");
+//        arrayList.add("quiz");
+//        arrayList.add("practice");
+//        arrayList.add("qa");
+//
+//        // Printing list before shuffling
+//        System.out.println("Original List : \n" + arrayList);
+//
+//        // Shuffling the list
+//        Collections.shuffle(arrayList);
+//
+//        // Printing list after shuffling
+//        System.out.println("\nShuffled List : \n" + arrayList);
+
+//
+//        10. Массив тизмесиндеги элементтерди тескери өзгөртүү үчүн Java программасын жазыңыз.
+//        ArrayList<String> colours = new ArrayList<>();
+//        colours.add("Red");
+//        colours.add("Green");
+//        colours.add("Orange");
+//        colours.add("White");
+//        colours.add("Black");
+//        System.out.println("List before reversing :\n" + colours);
+//        Collections.reverse(colours);
+//        System.out.println("List after reversing :\n" + colours);
+
+//
+//        11. Аррейлисттин бир бөлүгүн алуу үчүн Java программасын жазыңыз
+//        try {
+//            ArrayList<String> mentors = new ArrayList<>();
+//            //Addition of elements in ArrayList
+//            mentors.add("Maksat agai");
+//            mentors.add("Mukhammed agai");
+//            mentors.add("Rahim agai");
+//            mentors.add("Sakidin agai");
+//            mentors.add("Arzybek agai");
+//            mentors.add("Azamat agai");
+//
+//            System.out.println("Original ArrayList Content: " + mentors);
+//
+//            //Storing Sublist into another ArrayList
+//            ArrayList<String> arrayList = new ArrayList<>(mentors.subList(0,8));
+//            System.out.println("SubList stored in ArrayList: " + arrayList);
+//
+//        }
+//        catch (IndexOutOfBoundsException | IllegalArgumentException e) {
+//            System.out.println("Exception while getting sublist: " + e);
+//       }
+//
+//    }
+//}
+
+
+    }
+}
